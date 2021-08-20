a2304e20dc3a557c7370504708dcd1692d73293c
tim <tmsteem82@gmail.com>
2021-08-20 11:16:37 +0200

    implment retrieving information from AAVE token smart contract

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
 use std::str::FromStr;
 
-use web3::types::{H160, U256};
+use web3::contract::{Contract, Options};
+use web3::types::{Address, H160, U256};
 
 #[tokio::main]
 async fn main() -> web3::Result<()> {
@@ -23,6 +24,22 @@ async fn main() -> web3::Result<()> {
             balance.checked_div(wei_conv).unwrap()
         );
     }
+
+    let aave_addr = Address::from_str("0x42447d5f59d5bf78a82c34663474922bdf278162").unwrap();
+    let token_contract =
+        Contract::from_json(web3s.eth(), aave_addr, include_bytes!("erc20_abi.json")).unwrap();
+
+    let token_name: String = token_contract
+        .query("name", (), None, Options::default(), None)
+        .await
+        .unwrap();
+
+    let total_supply: U256 = token_contract
+        .query("totalSupply", (), None, Options::default(), None)
+        .await
+        .unwrap();
+
+    println!("Token name: {}, total supply: {}", token_name, total_supply);
 
     Ok(())
 }
